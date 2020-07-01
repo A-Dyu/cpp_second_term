bc2fcdecb94dee01c59812e6fad2400c392d2cdd
A-Dyu <58152674+A-Dyu@users.noreply.github.com>
2020-07-01 21:34:49 +0400

    fixes by Dogzik

--- bigint-optimized/shared_pointer.h
+++ bigint-optimized/shared_pointer.h
@@ -4,11 +4,68 @@
 #include <vector>
 
 struct shared_pointer {
-    shared_pointer(): ref_counter(1), v() {}
-    shared_pointer(std::vector<uint32_t> const& other): ref_counter(1), v(other) {}
-public:
+private:
     size_t ref_counter;
     std::vector<uint32_t> v;
+public:
+    shared_pointer(): ref_counter(1), v() {}
+
+    shared_pointer(std::vector<uint32_t> const& other): ref_counter(1), v(other) {}
+
+    friend void share(shared_pointer* &a, shared_pointer* b) {
+        a = b;
+        a->ref_counter++;
+    }
+
+    friend void unshare(shared_pointer* &a) {
+        if (a->ref_counter > 1) {
+            a->ref_counter--;
+            a = new shared_pointer(a->v);
+        }
+    }
+
+    friend void destroy(shared_pointer* a) {
+        a->ref_counter--;
+        if (a->ref_counter == 0) {
+            delete a;
+        }
+    }
+
+    size_t size() {
+        return v.size();
+    }
+
+    bool empty() {
+        return v.empty();
+    }
+
+    void push_back(uint32_t x) {
+        v.push_back(x);
+    }
+
+    void pop_back() {
+        v.pop_back();
+    }
+
+    void reverse() {
+        std::reverse(v.begin(), v.end());
+    }
+
+    uint32_t const& operator[](size_t i) const {
+        return v[i];
+    }
+
+    uint32_t& operator[](size_t i) {
+        return v[i];
+    }
+
+    uint32_t const& back() const {
+        return v.back();
+    }
+
+    uint32_t& back() {
+        return v.back();
+    }
 };
 
 
